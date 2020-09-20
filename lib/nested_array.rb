169c2a6ccbd50b872570f9479123a6e47ae68b44
Meredith McIntosh <meredith@mcintosh.io>
2020-09-20 20:02:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,5 +35,5 @@ matrix_lookup(assembled_matrix,1, 0)
 def matrix_update(matrix, row, column, new_value)
 matrix[row][column]= new_value
 end
-matrix_update(assembled_matrix,0,2,"Lemons")
+matrix_update(assembled_matrix,0,2,"Cats")
 binding.pry
